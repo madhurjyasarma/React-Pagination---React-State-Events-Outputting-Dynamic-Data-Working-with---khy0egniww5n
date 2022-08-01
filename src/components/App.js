48bb0d932b37da21a9be549ff938a9164de2b2d3
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2022-08-02 03:48:33 +0530

    React pagination

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import '../styles/App.css';
-import { PostList } from './PostList';
-const App = () => {
+import { render } from "enzyme";
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
 
+function App() {
+  const [para, setPara] = useState();
+  const changePara = () => {
+    return setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      <PostList />
+      {para && <p id="para">{para}</p>} 
+      <button id="click" onClick={changePara}>
+        clickMe
+      </button>
     </div>
-  )
+  );
 }
-
 
 export default App;
